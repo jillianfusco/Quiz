84611a11063d5b8bb4822cdc95b83cccf09863f7
Jillian Fusco <fuscojillian@gmail.com>
2024-09-23 21:05:20 -0400

    48:11 of carrie vid

---
+++ src/main/java/MultipleChoice.java
@@ -0,0 +1,17 @@
+
+
+public class MultipleChoice extends Question {
+
+    public MultipleChoice(String question, Choice[] choiceArray) {
+        super(question, choiceArray);
+    }
+
+    @Override
+    public String toString() {
+        String newLine = System.lineSeparator();
+        return newLine + "CHOOSE ONE:" + newLine +
+                getQuestion() + newLine +
+                getFormattedChoices();
+    }
+
+}
